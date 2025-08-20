be806904b3386ffdc0e79f203cc5833cde105f3c
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-19 23:32:06 -0300

    modifiqué estructura

--- CONDICIONALES/edadparavotar.js
+++ CONDICIONALES/edadparavotar.js
@@ -1,16 +1,18 @@
 /*Edad para votar
  Solicitar la edad del usuario y mostrar si puede votar*/
 
- let edad;
+let edad;
 
- edad = parseInt(prompt('Ingrese su edad:'));
+edad = parseInt(prompt('Ingrese su edad:'));
 
- if (edad >16 & edad <18){
+if (edad >= 16 && edad < 18) {
     document.writeln('Puede votar pero no es obligatorio')
- }
- else if (edad > 18 & edad < 70){
+}
+else if (edad >= 18 && edad <= 70) {
     document.writeln('Es Obligatorio votar')
- } else if (edad > 70){
+} else if (edad > 70) {
     document.writeln('El voto es optativo')
- }
-
+}
+else if (edad <16){
+    document.writeln('No puede votar')
+}

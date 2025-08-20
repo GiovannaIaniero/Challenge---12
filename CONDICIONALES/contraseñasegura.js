ca43955302967cd56ba5aebd68b0dd1bc23dda30
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-20 19:09:34 -0300

    termine estrucutra de ejercicio en contraseñasegura.js

--- CONDICIONALES/contraseñasegura.js
+++ CONDICIONALES/contraseñasegura.js
@@ -2,16 +2,16 @@
 Comparar una contraseña ingresada con una contraseña guardada. Mostrar
 mensaje si coinciden o no.*/
 
-let  ingresada = 123;
-let  guardada = 12345;
+let ingresada = 123;
+let guardada = 12345;
 
 ingresada = parseInt(prompt('Contraseña ingresada:'));
 guardada = parseInt(prompt('Contraseña guardad'));
 
-if (ingresada === guardada){
+if (ingresada === guardada) {
     document.writeln('Las contraseñas coinciden.')
 } else {
-    document.writeln('Las contraseñas no coincen')
+    document.writeln('Las contraseñas no coinciden.')
 }
 
 

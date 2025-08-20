6767fdfd278395e17f0e005e8c71bdc78733a341
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-19 22:21:09 -0300

    Desarrollo estrucrura

--- CONDICIONALES/positivonegativo.js
+++ CONDICIONALES/positivonegativo.js
@@ -1,3 +1,17 @@
 /*Número positivo o negativo
-○ Escribir un programa que determine si un número ingresado por el usuario es
+Escribir un programa que determine si un número ingresado por el usuario es
 positivo, negativo o cero*/
+
+let numero;
+
+numero = parseInt(prompt('Ingrese un número:'));
+
+if (numero > 0) {
+    document.writeln('Es un número positivo')
+}
+else if (numero < 0) {
+    document.writeln('Es un número negativo')
+} else if (numero === 0) {
+    document.writeln('El número ingresado es cero')
+}
+

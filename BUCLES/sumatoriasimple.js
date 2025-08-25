bb88e5e2d7a0d60c66dd5249474b708cad66a4c9
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-08-25 00:14:32 -0300

    termine estructura for

--- BUCLES/sumatoriasimple.js
+++ BUCLES/sumatoriasimple.js
@@ -1,3 +1,10 @@
 /*Sumatoria simple
 Calcular la suma de los primeros 50 números naturales.*/
 
+let suma = 0;
+
+for (let i = 1; i <= 50; i++) {
+    suma += i;
+}
+
+document.writeln("La suma es " + suma); 
